8cc5d9650291a7fa2ffe12cf77598af54bfbf711
Khaled gamal <khaled2000s.kg@gmail.com>
2022-05-19 14:42:04 +0200

    add book test

--- DB/DatabaseConnection.java
+++ DB/DatabaseConnection.java
@@ -3,12 +3,25 @@ package DB;
 import java.sql.*;
 
 public class DatabaseConnection {
-    Statement statement;
+
+     Statement statement;
     ResultSet resultSet;
     public void connect(){
         try{
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/library_schema","root","password");
             statement = connection.createStatement();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public void close(){
+        try{
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/library_schema","root","password");
+            connection.close();
+
         }catch (Exception e){
             e.printStackTrace();
         }
